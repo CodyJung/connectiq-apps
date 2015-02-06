627a9942f28ccfc4318428d490dec6424df0ef12
Cody Jung <cody@codyjung.com>
2015-02-05 22:33:08 -0600

    Move classpath stuff again
    Bash scripts don't actually affect the environment. Right.

--- scripts/build-pipboy.sh
+++ scripts/build-pipboy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export PATH=$PATH:connect-win-sdk-latest/bin/
+export CLASSPATH=$CLASSPATH:connect-win-sdk-latest/bin/monkeybrains.jar
+
 java -Dfile.encoding=UTF-8 com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d vivoactive
 
 java -Dfile.encoding=UTF-8 com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d epix
