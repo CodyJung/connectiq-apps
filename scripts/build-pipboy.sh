181b55bb29c5a4583328ab22540f7ba95944b92a
Cody Jung <cody@codyjung.com>
2015-02-05 23:22:33 -0600

    Try to generalize build scripts
    Bash loops. It's been a while, my old friend.

--- scripts/build-pipboy.sh
+++ scripts/build-pipboy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 pwd
 
-java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-sdk-win-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d vivoactive
-
-java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-sdk-win-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d epix
-
-java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-sdk-win-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d fr920xt
+for DEVICE in "${@:2}"; do
+java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/$1-$DEVICE.prg -w -z $1/resources/resources.xml -m $1/manifest.xml -u connect-sdk-win-latest/bin/devices.xml $1/source/* -d $DEVICE
+done
