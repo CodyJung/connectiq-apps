9544478460da268f3290ec66539f3b0322d4be3c
Cody Jung <cody@codyjung.com>
2015-02-05 22:01:06 -0600

    Attempting Travis CI
    Giving continuous integration a shot... probably will fail the
    first few times.

---
+++ scripts/build_pipboy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Dfile.encoding=UTF-8 -classpath connect-win-sdk-latest/bin/monkeybrains.jar; com.garmin.monkeybrains.Monkeybrains -a connect-win-sdk-latest/bin/api.db -i connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-win-sdk-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc 
---
+++ scripts/install-sdk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/7ewfu362h5chtt0/connect-sdk-win-latest.zip?dl=1 -O connect-win-sdk-latest.zip
+unzip -a connect-win-sdk-latest.zip
