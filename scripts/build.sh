ea82933b82a2672ca158f14b564597a6ebde6db1
Cody Jung <cody@codyjung.com>
2015-02-05 23:49:08 -0600

    Attempt to add auto-artifact uploads
    I'm not sure I got the yml right, though.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ev
 pwd
 
 for DEVICE in "${@:2}"; do
--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-wget https://www.dropbox.com/s/7ewfu362h5chtt0/connect-sdk-win-latest.zip?dl=1 -O connect-sdk-win-latest.zip
+wget https://s3.amazonaws.com/codyjung-ciq-builds/connect-sdk-win-latest.zip -O connect-sdk-win-latest.zip
 unzip -a connect-sdk-win-latest.zip
