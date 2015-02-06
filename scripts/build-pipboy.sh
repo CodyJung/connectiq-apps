7da8f0042954b510a9dc99a731509d03fc5a97aa
Cody Jung <cody@codyjung.com>
2015-02-05 22:42:47 -0600

    Seriously, with the classpaths?

--- scripts/build-pipboy.sh
+++ scripts/build-pipboy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo $PATH
-echo $CLASSPATH
+java -Dfile.encoding=UTF-8 -cp ../connect-win-sdk-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d vivoactive
 
-java -Dfile.encoding=UTF-8 com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d vivoactive
+java -Dfile.encoding=UTF-8 -cp ../connect-win-sdk-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d epix
 
-java -Dfile.encoding=UTF-8 com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d epix
-
-java -Dfile.encoding=UTF-8 com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d fr920xt
+java -Dfile.encoding=UTF-8 -cp ../connect-win-sdk-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a ../connect-win-sdk-latest/bin/api.db -i ../connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z ../PipBoy30/resources/resources.xml -m ../PipBoy30/manifest.xml -u ../connect-win-sdk-latest/bin/devices.xml ../PipBoy30/source/PipBoy30View.mc ../PipBoy30/source/PipBoy30App.mc -d fr920xt
