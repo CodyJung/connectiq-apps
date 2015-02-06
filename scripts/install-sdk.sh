6f6571cc891006bd55f6416fe47b2a66793a70b7
Cody Jung <cody@codyjung.com>
2015-02-05 22:28:31 -0600

    Try moving the classpath to install step

--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -ev
 wget https://www.dropbox.com/s/7ewfu362h5chtt0/connect-sdk-win-latest.zip?dl=1 -O connect-win-sdk-latest.zip
 unzip -a connect-win-sdk-latest.zip
+export PATH=$PATH:connect-win-sdk-latest/bin/
+export CLASSPATH=$CLASSPATH:connect-win-sdk-latest/bin/monkeybrains.jar
