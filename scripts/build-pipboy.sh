633af6ed9671c3fa409b5b89823a24864a4d89cf
Cody Jung <cody@codyjung.com>
2015-02-05 23:08:26 -0600

    Yep. Ok. Fix paths to be named correctly.
    ...

--- scripts/build-pipboy.sh
+++ scripts/build-pipboy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 pwd
-ls
 
-java -Dfile.encoding=UTF-8 -cp 'connect-sdk-win-latest/bin/monkeybrains.jar' com.garmin.monkeybrains.Monkeybrains -a connect-win-sdk-latest/bin/api.db -i connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-win-sdk-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d vivoactive
+java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-sdk-win-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d vivoactive
 
-java -Dfile.encoding=UTF-8 -cp 'connect-sdk-win-latest/bin/monkeybrains.jar' com.garmin.monkeybrains.Monkeybrains -a connect-win-sdk-latest/bin/api.db -i connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-win-sdk-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d epix
+java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-sdk-win-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d epix
 
-java -Dfile.encoding=UTF-8 -cp 'connect-sdk-win-latest/bin/monkeybrains.jar' com.garmin.monkeybrains.Monkeybrains -a connect-win-sdk-latest/bin/api.db -i connect-win-sdk-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-win-sdk-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d fr920xt
+java -Dfile.encoding=UTF-8 -cp connect-sdk-win-latest/bin/monkeybrains.jar com.garmin.monkeybrains.Monkeybrains -a connect-sdk-win-latest/bin/api.db -i connect-sdk-win-latest/bin/api.debug.xml -o out/PipBoy30.prg -w -z PipBoy30/resources/resources.xml -m PipBoy30/manifest.xml -u connect-sdk-win-latest/bin/devices.xml PipBoy30/source/PipBoy30View.mc PipBoy30/source/PipBoy30App.mc -d fr920xt
--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
-wget https://www.dropbox.com/s/7ewfu362h5chtt0/connect-sdk-win-latest.zip?dl=1 -O connect-win-sdk-latest.zip
-unzip -a connect-win-sdk-latest.zip
+wget https://www.dropbox.com/s/7ewfu362h5chtt0/connect-sdk-win-latest.zip?dl=1 -O connect-sdk-win-latest.zip
+unzip -a connect-sdk-win-latest.zip
